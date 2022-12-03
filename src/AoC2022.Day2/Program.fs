d87bbf83bd52ca23a7f92e9a9d3313f2fd1c71d6
Ian Griffiths <Ian.Griffiths@endjin.com>
2022-12-03 07:32:08 +0000

    Day 2 part 2

--- src/AoC2022.Day2/Program.fs
+++ src/AoC2022.Day2/Program.fs
@@ -13,7 +13,9 @@ let inputLines = getEmbeddedRows ()
 
 // Typed model of our input
 type Play = Rock | Paper | Scissors
-type StrategyEntry = StrategyEntry of predictedPlay:Play * recommendedPlay:Play
+type StrategyEntryPart1 = StrategyEntryPart1 of predictedPlay:Play * recommendedPlay:Play
+type Outcome = Lose | Draw | Win
+type StrategyEntryPart2 = StrategyEntryPart2 of predictedPlay:Play * requiredOutcome:Outcome
 
 // Input parsers
 let pPredictedPlay<'a> : Parser<Play, 'a> =
@@ -21,32 +23,50 @@ let pPredictedPlay<'a> : Parser<Play, 'a> =
     <|> (stringReturn "B" Paper)
     <|> (stringReturn "C" Scissors)
 
-let pRecommendedPlay<'a> : Parser<Play, 'a> =
+let pRecommendedPlayPart1<'a> : Parser<Play, 'a> =
         (stringReturn "X" Rock)
     <|> (stringReturn "Y" Paper)
     <|> (stringReturn "Z" Scissors)
 
-let pInputLine<'a> : Parser<StrategyEntry, 'a> =
+let pRecommendedPlayPart2<'a> : Parser<Outcome, 'a> =
+        (stringReturn "X" Lose)
+    <|> (stringReturn "Y" Draw)
+    <|> (stringReturn "Z" Win)
+
+let pInputLinePart1<'a> : Parser<StrategyEntryPart1, 'a> =
     pipe2
         pPredictedPlay
-        (spaces >>. pRecommendedPlay)
-        (fun predicted recommended -> StrategyEntry (predicted, recommended))
+        (spaces >>. pRecommendedPlayPart1)
+        (fun predicted recommended -> StrategyEntryPart1 (predicted, recommended))
 
+
+let pInputLinePart2<'a> : Parser<StrategyEntryPart2, 'a> =
+    pipe2
+        pPredictedPlay
+        (spaces >>. pRecommendedPlayPart2)
+        (fun predicted outcome -> StrategyEntryPart2 (predicted, outcome))
     
-let parseInputLine s = testp pInputLine s
-let parseInput rows = Seq.map parseInputLine rows
+let parseInputLinePart1 s = testp pInputLinePart1 s
+let parseInputPart1 rows = Seq.map parseInputLinePart1 rows
+let parseInputLinePart2 s = testp pInputLinePart2 s
+let parseInputPart2 rows = Seq.map parseInputLinePart2 rows
 
-let testGuide = parseInput testLines |> List.ofSeq
-let inputGuide = parseInput inputLines  |> List.ofSeq
-
+let testGuidePart1 = parseInputPart1 testLines |> List.ofSeq
+let inputGuidePart1 = parseInputPart1 inputLines  |> List.ofSeq
 
 // Test that our parsing works.
-testGuide.[0] =! StrategyEntry (Rock, Paper)
-testGuide.[1] =! StrategyEntry (Paper, Rock)
-testGuide.[2] =! StrategyEntry (Scissors, Scissors)
+testGuidePart1.[0] =! StrategyEntryPart1 (Rock, Paper)
+testGuidePart1.[1] =! StrategyEntryPart1 (Paper, Rock)
+testGuidePart1.[2] =! StrategyEntryPart1 (Scissors, Scissors)
 
+let testGuidePart2 = parseInputPart2 testLines |> List.ofSeq
+let inputGuidePart2 = parseInputPart2 inputLines  |> List.ofSeq
 
-type Outcome = Player1 | Draw | Player2
+testGuidePart2.[0] =! StrategyEntryPart2 (Rock, Draw)
+testGuidePart2.[1] =! StrategyEntryPart2 (Paper, Lose)
+testGuidePart2.[2] =! StrategyEntryPart2 (Scissors, Win)
+
+type RoundOutcome = Player1 | Draw | Player2
 let evaluateOutcome (play1:Play) (play2:Play) =
     match play1 with
     | Rock ->
@@ -69,7 +89,33 @@ evaluateOutcome Rock Paper =! Player2
 evaluateOutcome Rock Scissors =! Player1
 evaluateOutcome Rock Rock =! Draw
 
+let pickPlayForOutcome (outcome:Outcome) (play1:Play) =
+    match outcome with
+    | Outcome.Draw -> play1
+    | Win ->
+        match play1 with
+        | Rock -> Paper
+        | Paper -> Scissors
+        | Scissors -> Rock
+    | Lose ->
+        match play1 with
+        | Rock -> Scissors
+        | Paper -> Rock
+        | Scissors -> Paper
+
+
+pickPlayForOutcome Outcome.Draw Rock =! Rock
+pickPlayForOutcome Lose Paper =! Rock
+pickPlayForOutcome Win Scissors =! Rock
+
 type Round = Round of play1:Play * play2:Play
+
+let makeRoundFromPlay1ForOutcome (outcome:Outcome) (play1:Play) =
+    Round (play1, (pickPlayForOutcome outcome play1))
+
+makeRoundFromPlay1ForOutcome Outcome.Draw Rock =! Round (Rock, Rock)
+makeRoundFromPlay1ForOutcome Lose Paper =! Round (Paper, Rock)
+makeRoundFromPlay1ForOutcome Win Scissors =! Round (Scissors, Rock)
 
 let scoreShape play =
     match play with
@@ -99,11 +145,20 @@ scoreRound (Round (Rock, Paper)) =! (1, 8)
 scoreRound (Round (Paper, Rock)) =! (8, 1)
 scoreRound (Round (Scissors, Scissors)) =! (6, 6)
 
-let totalScore (guide:StrategyEntry list) =
+let totalScorePart1 (guide:StrategyEntryPart1 list) =
     guide
-    |> Seq.map (fun (StrategyEntry (opponent, me)) -> scoreRound (Round (opponent, me)))
+    |> Seq.map (fun (StrategyEntryPart1 (opponent, me)) -> scoreRound (Round (opponent, me)))
     |> Seq.sumBy (fun (opponentScore, myScore) -> myScore)
 
-totalScore testGuide =! 15
+totalScorePart1 testGuidePart1 =! 15
 
-printf "Part 1: %d" (totalScore inputGuide)
+let totalScorePart2 (guide:StrategyEntryPart2 list) =
+    guide
+    |> Seq.map (fun (StrategyEntryPart2 (opponent, outcome)) -> makeRoundFromPlay1ForOutcome outcome opponent)
+    |> Seq.map scoreRound
+    |> Seq.sumBy (fun (opponentScore, myScore) -> myScore)
+
+totalScorePart2 testGuidePart2 =! 12
+
+printf "Part 1: %d\n" (totalScorePart1 inputGuidePart1)
+printf "Part 2: %d\n" (totalScorePart2 inputGuidePart2)
